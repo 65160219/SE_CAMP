6722c14eadb7662542a3d60ae4b80734315ec94f
66160124 <152172067+65160219@users.noreply.github.com>
2023-12-21 18:23:40 +0700

    Update code class21dec66.php

--- class21dec66.php
+++ class21dec66.php
@@ -85,5 +85,12 @@
         <pre><?php print_r($arry);?></pre>   
         <pre><?php print_r($arry1);?></pre>      
         <pre><?php print_r($arry2);?></pre> 
+    <?php for($i = 0 ; $i< count($arry2); $i++){?>
+    <h3><?php echo $arry2[$i]; ?></h3>
+    <?php}?>
+
+    <?php foreach($arry2 as $key => $value){?>
+    <h3><?php echo $arry2[$key]; ?> : <?php echo $value; ?></h3>
+    <?php}?>
 </body>
 </html>
