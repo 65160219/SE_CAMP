40366d6de9a53d808c654a24f31b5bb027db2d5d
66160124 <152172067+65160219@users.noreply.github.com>
2023-12-21 17:57:33 +0700

    Update code class21dec66.php

--- class21dec66.php
+++ class21dec66.php
@@ -1,3 +1,4 @@
+<!--
 <html>
 
 <head>
@@ -28,3 +29,37 @@
 </body>
 
 </html>
+    -->
+
+<!--code class21dec66.php-->
+
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1><?php echo "Hello World<br><br>";?></h1>
+        <?php echo "Hello World<br><br></h1>"?>
+        <?php $var_a="SE CAMP"?>
+        <?php echo $var_b = 'Hello World $var_a'?>
+        <br>
+        <?php echo $var_c = "Hello World $var_a"?>
+        <br>
+        <?php 
+        $var_c += $var_b = 1
+        $var_c = '1';
+        if($var_b === $var_c){ 
+            //true
+            echo '$var_b === $var_c';
+        }else if($var_b <> $var_c){ 
+            //true
+            echo '$var_b <> $var_c';
+        }else{
+            //false
+            echo 'else';
+        } ?>
+        <br>
+        <?php echo $var_c = 2;?>
+        <br>
+        <?php echo $var_c?>
+    </body>
