fbf93089516452b5f7c186f036d3401996d61e2f
65160219 <152172067+65160219@users.noreply.github.com>
2024-02-12 17:12:19 +0700

    update code

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -102,22 +102,3 @@
 </html>
 -->
 
-<!doctype html>
-<html>
-
-<head>
-    <title>HOMEWORK-7</title>
-</head>
-<body>
-    <h1>ตารางสูตรคูณ</h1>
-    <input type="text" id="my_number" value="">
-    <button onclick="myFunction(my_number)">submit</button>
-    <script>
-    function myFunction(my_number){
-        for(let i=1;i<=10;i++){
-            console.log(1 + "x" + i + "=" + my_number*i);
-        }
-    }
-    </script>
-</body>
-</html>
